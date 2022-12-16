ce69192d82595615f5c2dc9d736d8fa730e119d2
Zakaria <zakariazainuddin41@yahoo.com>
2022-12-17 04:28:37 +0800

    ok

--- src/Component/UI/Button.jsx
+++ src/Component/UI/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
   return (
     <button 
-    className={`text-white rounded-xl bg-blue-400 w-20 h-8 cursor-pointer ${props.className}`} 
+    className={`text-white rounded-xl bg-blue-400 w-20 h-8 cursor-pointer k ${props.className}`} 
     onClick={props.onClick}
     disabled={props.disable}>
         {props.children}
